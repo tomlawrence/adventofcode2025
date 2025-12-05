74f3aa46f5c668ec14de4af173261b8aa75e39f3
Tom Lawrence <2392780+tomlawrence@users.noreply.github.com>
2025-12-05 00:14:49 +0000

    Day 04 complete

--- day04/day04.ts
+++ day04/day04.ts
@@ -10,9 +10,12 @@ const directions = [
 	[ 1, -1], [ 1, 0], [ 1, 1]
 ];
 
-const rolls: { row: number; col: number; adjacentRolls: number }[] = [];
 let part1 = 0;
-let part2 = 0;
+
+const adjacentRollsCount = Array.from({ length: warehouse.length }, () =>
+  Array(warehouse[0].length).fill(0),
+);
+const rollsToRecheck: [number, number][] = [];
 
 for (let row = 0; row < warehouse.length; row++) {
   for (let col = 0; col < warehouse[row].length; col++) {
@@ -27,10 +30,37 @@ for (let row = 0; row < warehouse.length; row++) {
 				if (warehouse[dRow][dCol] === "@") adjacentRolls++;
 			}
     }
-    rolls.push({ row, col, adjacentRolls });
-    if (adjacentRolls < 4) part1++;
+    adjacentRollsCount[row][col] = adjacentRolls;
+    if (adjacentRolls < 4) {
+      part1++;
+      rollsToRecheck.push([row, col]);
+    }
+  }
+}
+
+let part2 = 0;
+
+while (rollsToRecheck.length > 0) {
+  const [row, col] = rollsToRecheck.pop()!;
+  if (warehouse[row][col] !== "@") continue;
+  warehouse[row] =
+    warehouse[row].slice(0, col) + "x" + warehouse[row].slice(col + 1);
+  part2++;
+
+  for (const dir of directions) {
+    const dRow = row + dir[0];
+    const dCol = col + dir[1];
+    // prettier-ignore
+    if (dRow >= 0 && dRow < warehouse.length && dCol >= 0 && dCol < warehouse[row].length) {
+      if (warehouse[dRow][dCol] === "@") {
+        adjacentRollsCount[dRow][dCol]--;
+        if (adjacentRollsCount[dRow][dCol] < 4) {
+          rollsToRecheck.push([dRow, dCol]);
+        }
+      }
+    }
   }
 }
 
 console.log(`Part 1 Accessible rolls of paper: ${part1}`);
-console.log(`Part 2: ${part2}`);
+console.log(`Part 2 Number of rolls removed: ${part2}`);
