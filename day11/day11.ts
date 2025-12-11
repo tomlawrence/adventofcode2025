31f537f44239d08556aeb8e8ff6c8477c3c93450
Tom Lawrence <2392780+tomlawrence@users.noreply.github.com>
2025-12-11 20:55:42 +0000

    Day 11 complete

--- day11/day11.ts
+++ day11/day11.ts
@@ -1,15 +1,21 @@
 import { join } from "path";
 import { readInputFile } from "../helpers/readInputFile";
 import { logExecutionTimes } from "../helpers/logExecutionTimes";
+import { getSampleInput } from "../helpers/getSampleInput";
 
 const lines = readInputFile(join(__dirname, "day11.txt"));
 
 const p1Start = performance.now();
-const connections: Record<string, string[]> = {};
-for (const line of lines) {
-  const [device, outputs] = line.split(": ");
-  connections[device] = outputs.split(" ");
+
+function buildConnections(lines: string[]): Record<string, string[]> {
+  const connections: Record<string, string[]> = {};
+  for (const line of lines) {
+    const [device, outputs] = line.split(": ");
+    connections[device] = outputs.split(" ");
+  }
+  return connections;
 }
+let connections: Record<string, string[]> = buildConnections(lines);
 
 function countOuts(key: string): number {
   let count = 0;
@@ -22,5 +28,32 @@ function countOuts(key: string): number {
 const part1 = countOuts("you");
 const p1End = performance.now();
 
+const p2Start = performance.now();
+// Get day 11 part 2 sample if using part 1 sample
+if (lines[0] === "aaa: you hhh") {
+  connections = buildConnections(getSampleInput(11.2).split("\n"));
+}
+
+const visited = new Map<string, number>();
+function countPaths(key: string, seen: number): number {
+  if (key === "dac") seen += 1;
+  if (key === "fft") seen += 2;
+  if (key === "out") return seen === 3 ? 1 : 0;
+
+  const hash = `${key}:${seen}`;
+  if (visited.has(hash)) return visited.get(hash)!;
+
+  let total = 0;
+  for (const output of connections[key]) {
+    total += countPaths(output, seen);
+  }
+  visited.set(hash, total);
+  return total;
+}
+
+const part2 = countPaths("svr", 0);
+const p2End = performance.now();
+
 console.log(`Part 1 Total Paths From 'you' to 'out': ${part1}`);
-logExecutionTimes({ start: p1Start, end: p1End });
+console.log(`Part 2 Total Paths From 'svr' to 'out' via 'dac' and 'fft': ${part2}`);
+logExecutionTimes({ start: p1Start, end: p1End, p2Start: p2Start, p2End: p2End });
--- helpers/getSampleInput.ts
+++ helpers/getSampleInput.ts
@@ -100,6 +100,20 @@ ggg: out
 hhh: ccc fff iii
 iii: out`,
   };
-
+  if (day === 11.2) {
+    return `svr: aaa bbb
+aaa: fft
+fft: ccc
+bbb: tty
+tty: ccc
+ccc: ddd eee
+ddd: hub
+hub: fff
+eee: dac
+dac: fff
+fff: ggg hhh
+ggg: out
+hhh: out`;
+  }
   return samples[day] ?? `Sample data for day ${day} not available`;
 }
